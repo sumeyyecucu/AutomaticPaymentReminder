10759d84628bf28f22d3a38d57b7ed9256a8cfb3
sumeyyecucu <csumeyye150@gmail.com>
2026-05-13 18:53:30 +0300

    UpdateAsync changed as Update

--- AutomaticPaymentReminder.Application/Interfaces/IRepositories/IBaseRepo/IBaseWriteRepo.cs
+++ AutomaticPaymentReminder.Application/Interfaces/IRepositories/IBaseRepo/IBaseWriteRepo.cs
@@ -5,7 +5,7 @@ namespace AutomaticPaymentReminder.Application.Interfaces.IRepositories.IBaseRep
 public interface IBaseWriteRepo <T> : IRepo<T> where T : BaseEntity
 {
     Task<bool> AddAsync(T entity);
-    bool UpdateAsync(T entity);
+    bool Update(T entity);
     Task<bool> DeleteAsync(Guid id);
     Task SaveChangesAsync();
 
